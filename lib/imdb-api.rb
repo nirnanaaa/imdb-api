fa9327fdf11b47862aeaac170c737bc6806ac324
nirnanaaa <mosny@zyg.li>
2013-08-08 11:17:59 +0200

    added rakefile

--- lib/imdb-api.rb
+++ lib/imdb-api.rb
@@ -3,7 +3,21 @@
 require 'json'
 require 'open-uri'
 
+# == IMDB Api module definition
+#
+#
+# URL schemes:
+# Anonymization Service: anonymouse.org/cgi-bin/anon-www.cgi/
+# as IMDB bans IP's very fast
+#
+# Base URL: https://app.imdb.com
+#
+# Find URL: http://www.imdb.com/xml/find
+#
+#
 module ImdbApi
+  
+  BASE_URI = 'anonymouse.org/cgi-bin/anon-www.cgi/https://app.imdb.com'
   
   # == IMDB Api version String
   VERSION = '0.0.1'
--- lib/imdb-api/imdb.rb
+++ lib/imdb-api/imdb.rb
@@ -8,7 +8,16 @@ module ImdbApi
       # 
       # Performs the transition from /some/path into http(s)://some.url/some/path
       def build_url(path)
-        
+        default_params = {"api" => "v1", "appid" => "iphone1_1", "apiPolicy" => "app1_1", "apiKey" => "2wex6aeu6a8q9e49k7sfvufd6rhh0n", "locale" => "en_US", "timestamp" => Time.now.to_i}
+        query_param_array = []
+        base_uri = URI.parse(BASE_URI)
+        base_host = base_uri.host
+        the_path = base_uri.path + path
+        default_params.each_pair{|key, value| query_param_array << "#{key}=#{URI.escape(value.to_s)}" }
+        uri = URI::HTTP.build(:scheme => 'https', :host => base_host, :path => the_path, :query => query_param_array.join("&"))
+        query_param_array << "sig=app1-#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), default_params["apiKey"], uri.to_s)}"
+        uri = URI::HTTP.build(:scheme => 'https', :host => base_host, :path => the_path, :query => query_param_array.join("&"))
+        uri.to_s
       end
       
     end
