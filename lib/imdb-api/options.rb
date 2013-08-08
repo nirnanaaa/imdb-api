f169d3819616da134ff99b0a3d30de0d4fab2005
nirnanaaa <mosny@zyg.li>
2013-08-08 12:09:10 +0200

    added options

--- lib/imdb-api/options.rb
+++ lib/imdb-api/options.rb
@@ -5,9 +5,16 @@ module ImdbApi
       # == Class variable
       attr_accessor :options
       
+      def defaults
+        {
+          base_uri: 'https://app.imdb.com',
+          anonymize_uri: 'http://anonymouse.org/cgi-bin/anon-www.cgi/',
+          anonymize: false
+        }
+      end
       
       def set(options = {})
-        @options = ImdbApi::Object.new(options) if options.is_a? Hash
+        @options = ImdbApi::Object.new(defaults.merge(options)) if options.is_a? Hash
       end
       
     end
---
+++ spec/imdb-api/options_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe ImdbApi::Options do
+  before :each do
+    ImdbApi::Options.set({})
+  end
+  
+  it "should respond to options" do
+    expect(ImdbApi::Options).to respond_to(:options)
+  end
+  
+  it "should not be nil" do
+    expect(ImdbApi::Options.options).not_to be_nil
+  end
+  
+  [:base_uri, :anonymize_uri, :anonymize].each do |item|
+    it "responds to #{item}" do
+      expect(ImdbApi::Options.options).to respond_to(item)
+    end
+  end
+  
+end
